0211a29d01cd5ce5d2b60044cfaa7cc8088f3942
strange-tiger <jinolee9525@gmail.com>
2022-07-30 23:10:11 +0900

    Feat: 오브젝트 풀링 적용 - MovingObjectSpawner
    Log와 Vehicle - MovingObjectSpawner로 통합
    Tree와 WaterLily - StopObjectSpawner로 통합
    
    MovingObjectSpawner에 오브젝트 풀링 적용
    
    MapManager에 오브젝트 풀링 적용은 보류 - 유지보수 고려

---
+++ Crossy_Road_Copy/Assets/Scripts/Obstacles/MovingObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public float Speed = 10f;
+    public float MaintenanceTime;
+    
+    void Start()
+    {
+        MaintenanceTime = 120f / Speed;
+    }
+
+    private float _elapsedTime = 0f;
+    private void FixedUpdate()
+    {
+        transform.Translate(0f, 0f, Speed * Time.fixedDeltaTime);
+
+        _elapsedTime += Time.fixedDeltaTime;
+        if(_elapsedTime > MaintenanceTime)
+        {
+            _elapsedTime = 0f;
+            gameObject.SetActive(false);
+        }
+    }
+}
